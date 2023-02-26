6a3395b418a074b2b240e097ccd7beac35e3bf45
Andrei Piatsevich <apiats2@gmail.com>
2023-02-27 00:26:18 +0300

    add chat func

--- src/main/java/com/piatsevich/model/Chat.java
+++ src/main/java/com/piatsevich/model/Chat.java
@@ -1,4 +1,62 @@
 package com.piatsevich.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "chats")
 public class Chat {
+    @Id
+    @Column(name = "chat_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "chat_name")
+    private String name;
+
+//    @ManyToOne(fetch = FetchType.LAZY,
+//            cascade = { CascadeType.PERSIST, CascadeType.MERGE,
+//                    CascadeType.DETACH, CascadeType.REFRESH})
+//    @JoinColumn(name = "creator_id")
+//    private Employee employee;
+
+    public Chat() {
+    }
+
+    public Chat(Integer id, String name, Employee employee) {
+        this.id = id;
+        this.name = name;
+//        this.employee = employee;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public Employee getEmployee() {
+//        return employee;
+//    }
+//
+//    public void setEmployee(Employee employee) {
+//        this.employee = employee;
+//    }
+
+    @Override
+    public String toString() {
+        return "Chat{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", employee="  +
+                '}';
+    }
 }
---
+++ src/main/java/com/piatsevich/repository/ChatRepository.java
@@ -0,0 +1,6 @@
+package com.piatsevich.repository;
+
+import com.piatsevich.model.Chat;
+
+public interface ChatRepository extends GeneralRepository<Chat, Integer>{
+}
